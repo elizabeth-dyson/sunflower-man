11682b65560d3fb75793503cbf7ca221e39237c8
elizabeth-dyson <elizabethdyson810@gmail.com>
2025-07-31 09:01:42 -0500

    try again for the millionth time

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,15 +1,19 @@
+// supabaseServer.ts
 import { cookies } from 'next/headers';
 import { createServerClient } from '@supabase/ssr';
 
 export const createServerSupabaseClient = () => {
   const cookieStore = cookies();
 
+  // @ts-ignore
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get: (name) => cookieStore.get(name)?.value,
+        get(name: string) {
+          return cookieStore.get(name)?.value as string | undefined;
+        },
       },
     }
   );
